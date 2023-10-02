0b98cf55c9d602af4393b3a70d333ec6c9833447
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-10-02 06:34:51 -0700

    Added the view module for places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,7 +5,6 @@ from flask import make_response
 from flask import jsonify
 from flask import abort
 from flask import request
-
 from models import storage
 from models.place import Place
 from api.v1.views import app_views
@@ -20,7 +19,8 @@ def place():
     return jsonify([place.to_dict() for place in places.values()])
 
 
-@app_views.route("/places/<place_id>", methods=["GET"], strict_slashes=False)
+@app_views.route("/places/<place_id>",
+                 methods=["GET"], strict_slashes=False)
 def get_place(place_id):
     """Returns a single Place object"""
     place = storage.get(Place, place_id)
@@ -29,7 +29,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route("/places/<place_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route("/places/<place_id>",
+                 methods=["DELETE"], strict_slashes=False)
 def del_place(place_id):
     """Deletes a single Place Object from Storage"""
     place = storage.get(Place, place_id)
@@ -45,23 +46,23 @@ def create_place():
     """Creates an Place object and saves it to Storage"""
     req_data = request.get_json()
     if not req_data:
-        return jsonify({"message": "Not a JSON"}), 400
+        return make_response(jsonify({"message": "Not a JSON"}), 400)
     if "city_id" not in req_data:
-        return jsonify({"message": "Missing city_id"}), 400
+        return make_response(jsonify({"message": "Missing city_id"}), 400)
     city = storage.get(City, req_data["city_id"])
     if not city:
         abort(404)
     if "name" not in req_data:
-        return jsonify({"message": "Missing name"}), 400
+        return make_response(jsonify({"message": "Missing name"}), 400)
     if "user_id" not in req_data:
-        return jsonify({"message": "Missing user_id"}), 400
+        return make_response(jsonify({"message": "Missing user_id"}), 400)
     user = storage.get(User, req_data["user_id"])
     if not user:
         abort(404)
     place = Place(**req_data)
     storage.new(place)
     storage.save()
-    return jsonify(place.to_dict()), 201
+    return make_response(jsonify(place.to_dict()), 201)
 
 
 @app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
