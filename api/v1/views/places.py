bf74c153b445947e87c388f376942bb620cd7027
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-10-02 06:28:20 -0700

    Added the view module for places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,6 +9,8 @@ from flask import request
 from models import storage
 from models.place import Place
 from api.v1.views import app_views
+from models.city import City
+from models.user import User
 
 
 @app_views.route("/places", methods=["GET"], strict_slashes=False)
@@ -44,9 +46,19 @@ def create_place():
     req_data = request.get_json()
     if not req_data:
         return jsonify({"message": "Not a JSON"}), 400
-    elif "name" not in req_data:
+    if "city_id" not in req_data:
+        return jsonify({"message": "Missing city_id"}), 400
+    city = storage.get(City, req_data["city_id"])
+    if not city:
+        abort(404)
+    if "name" not in req_data:
         return jsonify({"message": "Missing name"}), 400
-    place = Place(name=req_data["name"])
+    if "user_id" not in req_data:
+        return jsonify({"message": "Missing user_id"}), 400
+    user = storage.get(User, req_data["user_id"])
+    if not user:
+        abort(404)
+    place = Place(**req_data)
     storage.new(place)
     storage.save()
     return jsonify(place.to_dict()), 201
@@ -62,7 +74,13 @@ def update_place(place_id):
     if not place:
         abort(404)
     for attr in place.to_dict():
-        if attr not in ['id', 'created_at', 'updated_at'] and attr in req_data:
+        if attr not in [
+            'id',
+            'created_at',
+            'updated_at',
+            'city_id',
+            'user_id'
+        ] and attr in req_data:
             setattr(place, attr, req_data[attr])
     storage.save()
     return jsonify(place.to_dict()), 200
