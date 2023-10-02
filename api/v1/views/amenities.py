907fc0d8cbbb3ba94d8777f1144a79887db0b89f
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-10-02 06:15:27 -0700

    Fixed the view module or amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -27,8 +27,7 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route("/amenities/<amenity_id>",
-                 methods=["DELETE"], strict_slashes=False)
+@app_views.route("/amenities/<amenity_id>", methods=["DELETE"], strict_slashes=False)
 def del_amenity(amenity_id):
     """Deletes a single Amenity Object from Storage"""
     amenity = storage.get(Amenity, amenity_id)
@@ -53,8 +52,7 @@ def create_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route("/amenities/<amenity_id>",
-                 methods=["PUT"], strict_slashes=False)
+@app_views.route("/amenities/<amenity_id>", methods=["PUT"], strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates an Amenity Object"""
     req_data = request.get_json()
